c20edc2426fa6a13ec73994652675a67874a5124
thaonx2890 <thaonx2890@gmail.com>
2011-11-02 05:33:41 +0000

    Add: protected function _initDb ()

--- Source3/application/Bootstrap.php
+++ Source3/application/Bootstrap.php
@@ -1,8 +1,20 @@
 <?php
-
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+    const KEY = 'CMS_Config';
+    protected function _initDb ()
+    {
+        if (! Zend_Registry::isRegistered($key)) {
+            $file = APPLICATION_PATH . DS . 'configs' . DS . 'application.ini';
+            $config = new Zend_Config_Ini($file);
+            Zend_Registry::set ( self::KEY , $config );
+        }
+       $host = $config->db->host;
+       $adapter = $config->db->adaptert;
+       $prefix = $config->db->params->prefix;
+       $username = $config->db->params->username;
+       $password = $config->db->params->password;
+       $dbname = $config->db->params->dbname;
+    }
 }
 
