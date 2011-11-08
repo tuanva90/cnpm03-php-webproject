ff136108aefdf83824db9e44f4772e2566b38ce1
thaonx2890@gmail.com <thaonx2890@gmail.com@05100a9a-ac78-c109-e75f-e7e8e921f437>
2011-11-08 03:21:01 +0000

    Modified function _initDb

--- trunk/Source3/application/Bootstrap.php
+++ trunk/Source3/application/Bootstrap.php
@@ -1,22 +1,17 @@
 <?php
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-//    const KEY = 'CMS_Config';
-//    protected function _initDb ()
-//    {
-//        if (! Zend_Registry::isRegistered(self::KEY)) {
-//            $file = APPLICATION_PATH . DS . 'configs' . DS . 'application.ini';
-//            $config = new Zend_Config_Ini($file);
-//           Zend_Registry::set(self::KEY, $config);
-//       }
-//       $host = $config->db->host;
-//       $adapter = $config->db->adaptert;
-//       $prefix = $config->db->params->prefix;
-//       $username = $config->db->params->username;
-//       $password = $config->db->params->password;
-//       $dbname = $config->db->params->dbname;
+    const KEY = 'CMS_Config';
+    protected function _initDb ()
+    {
+        if (! Zend_Registry::isRegistered(self::KEY)) {
+            $file = APPLICATION_PATH . DS . 'configs' . DS . 'application.ini';
+            $config = new Zend_Config_Ini($file);
+            // Store Config to use for other Controller 
+            Zend_Registry::set(self::KEY, $config);
+        }
 //        $dbOption = $config->db->toArray();
-//        // Initialize database
+//      // Initialize database
 //        $db = Zend_Db::factory($dbOption['adapter'], 
 //        $dbOption['params']);
 //        $db->setFetchMode(Zend_Db::FETCH_ASSOC);
@@ -28,6 +23,6 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 //        Zend_Registry::set('connectDB', $db);
 //        // Return it, so that it can be stored by the bootstrap
 //        return $db;
-//    }
+    }
 }
 
