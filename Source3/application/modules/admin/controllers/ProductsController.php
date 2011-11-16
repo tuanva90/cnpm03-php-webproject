28349e96653074d48bae476f1cc526402212c07c
huynvk <huynvk@gmail.com>
2011-11-16 13:05:01 +0000

    Edit productnewAction

--- Source3/application/modules/admin/controllers/ProductsController.php
+++ Source3/application/modules/admin/controllers/ProductsController.php
@@ -94,8 +94,86 @@ class Admin_ProductsController extends Honey_Controller_Action {
     	$id = $this->_request->getParam('id');
     	$this->view->id=$id;
     }
-	public function productnewAction(){		
-	}
-	public function producteditAction(){
-	}
+	
+    public function deleteAction ()
+    {
+        $id = $this->_request->getParam('id');
+        $this->view->id = $id;
+    }
+    public function updateAction ()
+    {
+        $id = $this->_request->getParam('id');
+        $this->view->id = $id;
+    }
+    
+    /**
+     * productnewAction
+     * Excute action for productnewController.
+     * Edited by: HuyNVK
+     * Modified Date: 14/11/2011
+     * Modified Content: Add Body for Function.
+     */
+    public function productnewAction () {
+        //Create an Admin_Form_ProductNew()
+        $form = new Admin_Form_ProductNew();
+        $style_error = '<p style="background:#FF0000; text-align: center; padding: 3px;">';
+        $style_success = '<p style="background:#33CC66; text-align: center; padding: 3px;">';
+        $message = '';
+        
+        if ($this->_request->isPost()) {
+            if ($form->isValid($_POST)) {
+            	//Get value posted
+            	$result = $form->getValues();
+            	$name = $result['name'];
+                $description = $result['description'];
+                $catalog = $result['catalog'];
+                $model = $result['model'];
+                $image = $result['image'];
+                $price = $result['price'];
+                $order = $result['order'];
+                $status = $result['status'];
+                
+                //Check whether the values is valid or not
+                if($name == "") {
+                	$message = $style_error . 'Chưa nhập tên sản phẩm.</p>';
+                } else if($description == "") {
+                	$message = $style_error . 'Chưa nhập mô tả sản phẩm.</p>';
+                } else if($model == "") {
+                	$message = $style_error . 'Chưa nhập model sản phẩm.</p>';
+                } else if($price == "") {
+                	$message = $style_error . 'Chưa nhập giá sản phẩm.</p>';
+                } else {
+	                
+                	//Insert data to database
+	                $productModel = new Admin_Model_Products();
+	                $now = getdate();
+	                $viewed = 1;
+	                $productModel->InsertProduct($model, $image, $price, $now, $now, $now, $viewed, $order, $status, $name, $description);
+	                
+                	//Show message
+	                $message = $style_success . 'Đã thêm '.$name.' vào cơ sở dữ liệu</p>';
+	                
+                	
+                	/*
+	                //Test data
+	                $message = $style_success . 'Name: ' . $name . '<br/>';
+	                $message = $message . 'Description: ' . $description . '<br/>';
+	                $message = $message . 'Catalog: ' . $catalog . '<br/>';
+	                $message = $message . 'Model: ' . $model . '<br/>';
+	                $message = $message . 'Image: ' . $image . '<br/>';
+	                $message = $message . 'Price: ' . $price . '<br/>';
+	                $message = $message . 'Order: ' . $order . '<br/>';
+	                $message = $message . 'Status: ' . $status . '<br/>';
+	                */
+                }
+            } else {
+                $message = $style_error . 'An Error Occurred.</p>';
+            }
+        }
+        
+        $this->view->note = $message;
+        $this->view->form = $form;
+    }
+    public function producteditAction ()
+    {}
 }
