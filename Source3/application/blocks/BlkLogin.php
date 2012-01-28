b07720cb600137d2e21b3a29e6e495f5fa0caae3
huynvk <huynvk@gmail.com>
2012-01-28 13:46:24 +0000

    Edit và Save option cho module Login.

--- Source3/application/blocks/BlkLogin.php
+++ Source3/application/blocks/BlkLogin.php
@@ -1,10 +1,17 @@
 <?php
 class Block_BlkLogin extends Zend_View_Helper_Abstract{
 
-	public function BlkLogin (){
+	public function BlkLogin ($info){
 		
 		$view  = $this->view;
 		$arrParam = $view->arrParam;
+		
+		//Setting default value
+		$use_forgot_password_link = 0;
+		$use_keep_signed_in = 0;		
+		
+		//Setting module-option
+		eval($info['option']);
 		
 		require_once(APPLICATION_PATH . DS . DS . 'blocks' . DS. 'BlkLogin' . DS . 'default.php');
 	}
--- Source3/application/blocks/BlkNewestProduct/default.php
+++ Source3/application/blocks/BlkNewestProduct/default.php
@@ -15,9 +15,6 @@ $(function(){
 			Cancel: function() {
 				$( this ).dialog( "close" );
 			}
-		},
-		close: function() {
-			allFields.val( "" ).removeClass( "ui-state-error" );
 		}
 	});
 });
