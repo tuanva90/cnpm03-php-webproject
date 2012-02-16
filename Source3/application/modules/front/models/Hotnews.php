8ff3e5fbd9bcb7d1be0a880b49dd9e1f63075d31
hoangduc17t <hoangduc17t@gmail.com>
2012-02-16 06:07:35 +0000

    update hotnews

--- Source3/application/modules/front/models/Hotnews.php
+++ Source3/application/modules/front/models/Hotnews.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php 
 /**
  * @author: DucNH
  * @Class: Front_Model_Hotnews
@@ -17,31 +17,49 @@ class Front_Model_Hotnews extends Honey_Db_Table{
 		$paginator = $arrParam['paginator'];
 		if($option['task'] == 'listAll')
 		{
-			$select = $db->select()
-			->from(array('hn' => $this->getPrefix().'hotnews'))
-			->joinLeft(array('n' => $this->getPrefix().'news', 'hn.news_id = n.news_id'))
-			->joinLeft(array('nd' => $this->getPrefix().'news_description', 'hn.news_id = nd.news_id'))
-			->where('nd.language = ?', $this->_lang)
-			->order('hn.date_added DESC');
+			$select = "SELECT * FROM ".$this->getPrefix()."hotnews as hn LEFT JOIN ".$this->getPrefix()."news as n ON n.news_id = hn.news_id LEFT JOIN ".$this->getPrefix()."news_description as nd ON nd.news_id = hn.news_id WHERE nd.language = '".$this->_lang."' AND n.status = 1 ORDER BY hn.date_added ASC";
+			
 			if ($paginator['itemCountPerPage'] > 0){
 				$page = $paginator['currentPage'];
 				$rowCount = $paginator['itemCountPerPage'];
-				$select->limitPage($page,$rowCount);
+				$startindex = ($page - 1) * $rowCount;
+				$select = $select.' LIMIT '.$startindex.','.$rowCount;
 			}
 			$result = $db->fetchAll($select);
 			return $result;
 		}
-		if($option['task'] == 'listtop')
+		if($option['task'] == 'listTop')
 		{
-			$select = $db->select()
-			->from(array('hn' => $this->getPrefix().'hotnews'))
-			->joinLeft(array('n' => $this->getPrefix().'news', 'hn.news_id = n.news_id'))
-			->joinLeft(array('nd' => $this->getPrefix().'news_description', 'hn.news_id = nd.news_id'))
-			->where('nd.language = ?', $this->_lang)
-			->order('hn.date_added DESC')
-			->limit(0,5);
+			$select = "SELECT * FROM ".$this->getPrefix()."hotnews as hn LEFT JOIN ".$this->getPrefix()."news as n ON n.news_id = hn.news_id LEFT JOIN ".$this->getPrefix()."news_description as nd ON nd.news_id = hn.news_id WHERE nd.language = '".$this->_lang."' AND n.status = 1 ORDER BY hn.date_added ASC limit 0,5";
 			$result = $db->fetchAll($select);
 			return $result;
+		}
+	}
+	
+	public function countItem($arrParam = null, $option = null){
+	    $db = Zend_Registry::get('connectDB');
+	    if($option['task'] == 'countAll')
+	    {
+	        $select = $db->select()
+	        	->from($this->getPrefix().'hotnews', array('COUNT(*) as hotnewscount'));
+	        
+	        $result = $db->fetchOne($select);
+	        return $result;
+	    }
+	}
+	
+	public function saveItem($arrParam = null, $option = null)
+	{
+	    $db = Zend_Registry::get('connectDB');
+	    
+	    if($option['task'] == 'add')
+	    {
+	        $i_data = array(
+	        	'news_id' => $arrParam['news_id'],
+	        	'date_added' => $arrParam['date']
+	        );
+	        
+	        $db->insert($this->getPrefix().'hotnews', $i_data);
 		}
 	}
 }
