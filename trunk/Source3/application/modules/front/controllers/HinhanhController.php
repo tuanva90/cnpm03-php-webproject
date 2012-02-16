4e04648f65a1bcb93cff9d70bd818e90e1bf0c97
hikaruit15@gmail.com <hikaruit15@gmail.com@05100a9a-ac78-c109-e75f-e7e8e921f437>
2012-02-16 15:51:09 +0000

    Updater HinhanhController.php

---
+++ trunk/Source3/application/modules/front/controllers/HinhanhController.php
@@ -0,0 +1,38 @@
+<?php
+class HinhanhController extends Honey_Controller_Action {
+	
+	//Parameter array received in any Action
+	protected $_arrParam;
+	
+	//The path of the Controller
+	protected $_currentController;
+	
+	//The path of the Action
+	protected $_actionMain;
+	
+	//Paging parameters
+	protected $_paginator;
+	
+	public function init() {
+		/** View */
+		$this->view->arrParam = $this->_arrParam;
+		$this->view->currentController = $this->_currentController;
+		$this->view->actionMain = $this->_actionMain;
+		
+		$layout =  'index';
+		$layoutPath = APPLICATION_PATH . '/templates/front/default';
+		$this->loadTemplate ($layout, $layoutPath, 'template.ini', 'template' );
+		
+	}
+	
+	public function indexAction() {
+		
+		
+
+	}
+	
+	public function detailAction() {
+		
+		}
+
+}
